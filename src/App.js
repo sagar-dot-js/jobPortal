ec519cfda445faa9f1a008600aae876dbd27a35f
sagar-dot-js <110444387+sagar-dot-js@users.noreply.github.com>
2022-09-20 11:14:29 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,68 +1,29 @@
-import { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import Login from "./pages/Login";
-import { ReactSession } from "react-client-session";
-import Axios from "axios";
+import React, { useState } from "react";
+var geolocation = require("geolocation");
 
-function App() {
-  let [loggedIn, setLoggedIn] = useState({
-    loginStatus: false,
+const App = () => {
+  let [location, setLocation] = useState();
+
+  geolocation.getCurrentPosition(function (err, position) {
+    if (err) throw err;
+
+    setLocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
   });
 
-  let [tokenExist, setTokenExist] = useState(false);
-
-  let base = "http://localhost:3001";
-
-  ReactSession.setStoreType("localStorage");
-
-  const token = ReactSession.get("token");
-  console.log(token);
-
-  let checkTokenInSession = () => {
-    Axios.get(`${base}/api/v1/findSession`).then((result) => {
-      console.log(result?.data?.jwtToken);
-      if (token === result?.data?.jwtToken) {
-        console.log("Match");
-        setTokenExist(true);
-      } else {
-        console.log("Not match");
-      }
-    });
-  };
-
-  let logOut = () => {
-    console.log("Log Out");
-    ReactSession.set("token", "");
-  };
-
-  useEffect(() => {
-    checkTokenInSession();
-  }, []);
-
+  console.log(location);
   return (
-    <>
-      <div className="text-2xl">Job Portal</div>
-      <div className="text-blue-600  w-full h-screen flex items-center justify-center">
-        {!tokenExist ? (
-          <div>
-            {loggedIn.loginStatus ? (
-              `Already logged In Job Portal ${loggedIn?.userData}`
-            ) : (
-              <Login setLoggedIn={setLoggedIn} loggedIn={loggedIn} />
-            )}
-          </div>
-        ) : (
-          <p
-            onClick={() => {
-              logOut();
-            }}
-          >
-            Log Out
-          </p>
-        )}
-      </div>
-    </>
+    <div className="h-screen w-full">
+      <p>
+        Lat: <span>{location?.latitude} </span>
+      </p>
+      <p>
+        Long <span>{location?.longitude} </span>
+      </p>
+    </div>
   );
-}
+};
 
 export default App;
